b0d801e2ae12ee104a8fcd413325c28b7e642e59
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-09-08 14:57:44 -0500

    added create method for VolunteerHours getting everything from params

--- app/controllers/volunteer_hours_controller.rb
+++ app/controllers/volunteer_hours_controller.rb
@@ -3,4 +3,18 @@ class VolunteerHoursController < ApplicationController
     @volunteer_hours = VolunteerHour.all
     render :index
   end
+
+  def create
+    @volunteer_hour = VolunteerHour.create(
+      user_id: params[:user_id],
+      event_id: params[:event_id],
+      hours: params[:hours],
+      date: params[:date]
+    )
+    if @volunteer_hour.save
+      render :show
+    else
+      render json: {errors: @volunteer_hour.errors.full_messages}, status: :bad_request
+    end
+  end
 end
--- test/controllers/volunteer_hours_controller_test.rb
+++ test/controllers/volunteer_hours_controller_test.rb
@@ -8,4 +8,16 @@ class VolunteerHoursControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal VolunteerHour.count, data.length
   end
+
+  test "create" do
+    assert_difference "VolunteerHour.count", 1 do
+      post "/volunteer_hours.json", params: {
+        user_id: User.first.id,
+        event_id: Event.first.id,
+        hours: 5,
+        date: DateTime.now
+      }
+      assert_response 200
+    end
+  end
 end
