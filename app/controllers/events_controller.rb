2fd702ca80ce84d6334eb24004eaac097871ee5a
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-09-08 10:42:25 -0500

    added event create action and test

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,17 @@ class EventsController < ApplicationController
     @events = Event.all
     render :index
   end
+
+  def create
+    @event = Event.create(
+      name: params[:name],
+      date: params[:date],
+      description: params[:description]
+    )
+    if @event.save
+      render :show
+    else
+      render json: {errors: @event.errors.full_messages}, status: :bad_request
+    end
+  end
 end
--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -8,4 +8,15 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Event.count, data.length
   end
+
+  test "create" do
+    assert_difference "Event.count", 1 do
+      post "/events.json", params: {
+        name: "Test Event",
+        date: DateTime.now,
+        description: "this is a description of an event"
+      }
+      assert_response 200
+    end
+  end
 end
