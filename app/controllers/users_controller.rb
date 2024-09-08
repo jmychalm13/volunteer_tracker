b8e6897e42490f4aff6608138e496bc2d2230a7f
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-09-08 10:26:54 -0500

    added active column with a default of true and jbuilder for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,19 +5,12 @@ class UsersController < ApplicationController
       email: params[:email],
       password: params[:password],
       password_confirmation: params[:password_confirmation],
-      role: params[:role]
+      role: params[:role],
     )
     if @user.save
-      render :show
+      render :show, status: :created
     else
       render json: {errors: @user.errors.full_messages}, status: :bad_request
     end
-  end
-
-  def update
-    @user = User.find_by(id: params[:id])
-    @user.update(
-      
-    )
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_08_151630) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_08_152311) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_08_151630) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role"
-    t.boolean "active"
+    t.boolean "active", default: true
   end
 
 end
